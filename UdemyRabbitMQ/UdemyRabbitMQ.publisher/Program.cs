893c98a1fe0bc080ac8b222061df681916ae63b7
Erhan Basturk <56194980+basturkerhan@users.noreply.github.com>
2022-09-26 13:13:42 +0300

    Initial commit

---
+++ UdemyRabbitMQ/UdemyRabbitMQ.publisher/Program.cs
@@ -0,0 +1,53 @@
+﻿using RabbitMQ.Client;
+using System;
+using System.Linq;
+using System.Text;
+
+namespace UdemyRabbitMQ.publisher
+{
+    public enum LogNames
+    {
+        Critical=1,
+        Error=2,
+        Warning=3,
+        Info=4,
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var uri = Environment.GetEnvironmentVariable("URI", EnvironmentVariableTarget.Process);
+            var factory = new ConnectionFactory();
+            factory.Uri = new Uri(uri);
+
+            using var connection = factory.CreateConnection();
+
+            var channel = connection.CreateModel();
+
+            channel.ExchangeDeclare("logs-direct", durable:true, type:ExchangeType.Direct);
+
+            Enum.GetNames(typeof(LogNames)).ToList().ForEach(x =>
+            {
+                var rootKey = $"route-{x}";
+                var queueName = $"direct-queue-{x}";
+                channel.QueueDeclare(queueName, true, false, false);
+                channel.QueueBind(queueName, "logs-direct", rootKey);
+            });
+
+            Enumerable.Range(1, 50).ToList().ForEach(x =>
+            {
+                LogNames logName = (LogNames) new Random().Next(1, 5);
+                string message = $"Log: {x} and Type: {logName}";
+                var messageBody = Encoding.UTF8.GetBytes(message); // kuyruğa bayt olarak gönderilir
+
+                var rootKey = $"route-{logName}";
+                channel.BasicPublish("logs-direct", rootKey, null, messageBody);
+
+                Console.WriteLine($"Log Gönderildi: {message}");
+            });
+  
+            Console.ReadLine();
+        }
+    }
+}
---
+++ UdemyRabbitMQ/UdemyRabbitMQ.subscriber/Program.cs
@@ -0,0 +1,42 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace UdemyRabbitMQ.subscriber // consumer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var uri = Environment.GetEnvironmentVariable("URI", EnvironmentVariableTarget.Process);
+            var factory = new ConnectionFactory();
+            factory.Uri = new Uri(uri);
+            using var connection = factory.CreateConnection();
+            var channel = connection.CreateModel();
+
+            channel.BasicQos(0, 1, false); // mesajlar kuyruktan kaçar kaçar gelecek? videodan parametreleri dinle
+            var consumer = new EventingBasicConsumer(channel);
+
+            var queueName = "direct-queue-Warning";
+            channel.BasicConsume(queueName, false, consumer);
+            // ikinci parametre true verilirse rabbitmq mesaj yanlış da alınsa doğru da alınsa kuyruktan siler
+            // false yaparsak ben sana mesajı doğru gönderdiğini haber edeceğim o zaman sil diyoruz
+
+            Console.WriteLine("Loglar Dinleniyor...");
+            consumer.Received += (object sender, BasicDeliverEventArgs e) =>
+            {
+                var message = Encoding.UTF8.GetString(e.Body.ToArray());
+                Thread.Sleep(500);
+                Console.WriteLine("Kuyruktan Gelen Mesaj: " + message);
+
+                channel.BasicAck(e.DeliveryTag, false); // mesajı artık kuyruktan silebilirsin diye belirttik (yukarıyı false yaptığımız için)
+            };
+
+            Console.ReadLine();
+        }
+
+    }
+}
